ec6fe8323824a1830000eea4cd2fc905a6570583
aninha3 <106088830+aninh4yx@users.noreply.github.com>
2023-11-09 08:47:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert( comi um takaka)
+function tocaSom(idElementoAudio){
+document.querySelector(idllementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    cons+
+    t iAudio = "som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom (idAudio)
+    }
+    contador = contador + 1;
+    console.log(contador);
+}
